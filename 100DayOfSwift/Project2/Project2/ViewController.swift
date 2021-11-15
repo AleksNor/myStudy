6256ccbca7cbd010de5ea2e3a5510fa4b2eb291e
Evgeniy Karpov <56111469+JohnnyThompson@users.noreply.github.com>
2021-11-15 08:44:40 +0300

    Project 2 Day#20

--- 100DayOfSwift/Project2/Project2/ViewController.swift
+++ 100DayOfSwift/Project2/Project2/ViewController.swift
@@ -15,11 +15,13 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
         askQuestion()
         
         button1.layer.borderWidth = 1
@@ -31,12 +33,30 @@ class ViewController: UIViewController {
         button3.layer.backgroundColor = UIColor(red: 0.93, green: 0.94, blue: 0.94, alpha: 1).cgColor
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        correctAnswer = Int.random(in: 0...2)
+        countries.shuffle()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        title = countries[correctAnswer].uppercased()
     }
 
-
+    @IBAction func tappedButton(_ sender: UIButton) {
+        var title: String
+        
+        if correctAnswer == sender.tag {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        self.present(ac, animated: true, completion: nil)
+    }
+    
 }
 
