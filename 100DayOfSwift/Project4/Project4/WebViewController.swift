13261a81833de247a6f45671a350838a9cbd4454
Evgeniy Karpov <56111469+JohnnyThompson@users.noreply.github.com>
2021-11-21 11:04:36 +0300

    README update

---
+++ 100DayOfSwift/Project4/Project4/WebViewController.swift
@@ -0,0 +1,99 @@
+//
+//  WebViewController.swift
+//  Project4
+//
+//  Created by Евгений Карпов on 21.11.2021.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController,WKNavigationDelegate {
+    
+    var websites = ["google.com", "hackingwithswift.com", "yandex.ru"]
+    var webView: WKWebView!
+    var progressView: UIProgressView!
+    var firstURL: URL = URL(string: "https://yandex.ru")!
+    
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.load(URLRequest(url: firstURL))
+        webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        let goBack = UIBarButtonItem(title: "Back", style: .plain, target: webView, action: #selector(webView.goBack))
+        let goForward = UIBarButtonItem(title: "Forward", style: .plain, target: webView, action: #selector(webView.goForward))
+
+        toolbarItems = [progressButton, spacer, goBack, goForward, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+
+    }
+    
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+
+        decisionHandler(.cancel)
+        showAlertMessage()
+    }
+    
+    private func showAlertMessage() {
+        let ac = UIAlertController(title: "Access denied", message: "This URL is blocked", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default))
+        self.present(ac, animated: true, completion: nil)
+    }
+    
+
+
+}
--- README.md
+++ README.md
@@ -2,9 +2,18 @@
 Репозитория содержащяя все мои практические задания за время обучения
 ## Оглавление
 - [100 Day Of Swift](#100DayOfSwift)
+- [SwiftBook](#SwiftBook)
 
 
 ## <a id="100DayOfSwift"></a>100 Day Of Swift
-- [Project 1](100DayOfSwift/Project1/README.md)
-- [Project 2](100DayOfSwift/Project2)
+- [Day 16-18, Project 1](100DayOfSwift/Project1/README.md)
+- [Day 19-21, Project 2](100DayOfSwift/Project2/README.md)
+- [Day 22, Project 3](100DayOfSwift/Project3)
+- [Day 23, Milestone: Projects 1-3](100DayOfSwift/ChallengeD23)
+- [Day 24-26, Project 4](100DayOfSwift/Project4)
+
+## <a id="SwiftBook"></a>SwiftBook
+- [Block #2]
+    - [Home Work #1](swiftbook/Home Work 2.1.)
+    - [Home Work #1](swiftbook/Home Work 2.2.)
 
