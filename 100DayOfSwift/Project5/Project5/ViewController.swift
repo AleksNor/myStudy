5c0669692edfab88bfbc31a35b1d1b02f6527d41
Evgeniy Karpov <56111469+JohnnyThompson@users.noreply.github.com>
2021-11-24 18:20:25 +0300

    Day 29 Wrap up

--- 100DayOfSwift/Project5/Project5/ViewController.swift
+++ 100DayOfSwift/Project5/Project5/ViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var allWords = [String]()
-    var usedWords = [String]()
+    //MARK: - Private properties
+    
+    private var allWords = [String]()
+    private var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +33,9 @@ class ViewController: UITableViewController {
         startGame()
     }
     
-    func startGame() {
+    //MARK: - Private func
+    
+    private func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -39,16 +43,6 @@ class ViewController: UITableViewController {
     
     @objc func restartGame() {
         startGame()
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return usedWords.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
-        cell.textLabel?.text = usedWords[indexPath.row]
-        return cell
     }
     
     @objc func promptForAnswer() {
@@ -64,7 +58,7 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
      }
     
-    func submit(_ answer: String) {
+    private func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
 
         let errorTitle: String
@@ -73,7 +67,7 @@ class ViewController: UITableViewController {
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
-                    usedWords.insert(answer, at: 0)
+                    usedWords.insert(answer.lowercased(), at: 0)
 
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
@@ -92,13 +86,11 @@ class ViewController: UITableViewController {
             errorTitle = "Word not possible"
             errorMessage = "You can't spell that word from \(title)"
         }
-
-        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
+        
+        showErrorAlert(titile: errorTitle, message: errorMessage)
     }
     
-    func isPossible(word: String) -> Bool {
+    private func isPossible(word: String) -> Bool {
         guard var tempWord = title?.lowercased() else { return false }
 
         for letter in word {
@@ -111,20 +103,41 @@ class ViewController: UITableViewController {
 
         return true
     }
-
-    func isOriginal(word: String) -> Bool {
+    
+    private func isOriginal(word: String) -> Bool {
         return !usedWords.contains(word)
     }
 
-    func isReal(word: String) -> Bool {
+    private func isReal(word: String) -> Bool {
         let checker = UITextChecker()
-            let range = NSRange(location: 0, length: word.utf16.count)
-            let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-
-            return misspelledRange.location == NSNotFound
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        guard word.count > 2 else {
+            return false
+        }
+        return misspelledRange.location == NSNotFound
     }
+    
+    //MARK: - Show Allert
+    
+    private func showErrorAlert(titile: String, message: String) {
+        let ac = UIAlertController(title: titile, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true, completion: nil)
+    }
+}
 
+//MARK: - TabelView settings
 
-
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 }
 
