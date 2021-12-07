b3fe9e55d2a97a4e0db1a6e73b66f62b5d2feec3
Evgeniy Karpov <56111469+JohnnyThompson@users.noreply.github.com>
2021-12-07 16:07:58 +0300

    Day 42 Project 10/1

---
+++ 100DayOfSwift/Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Евгений Карпов on 07.12.2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
