ca913cdbc6173997381de16358cc4c311ced7525
Evgeniy Karpov <56111469+JohnnyThompson@users.noreply.github.com>
2021-12-29 23:15:14 +0300

    Day 64 Project 18

---
+++ 100DayOfSwift/Project18/Project18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Евгений Карпов on 29.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+    }
+}
+
