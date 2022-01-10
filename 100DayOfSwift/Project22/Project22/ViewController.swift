c9f1753d995125da9bee2ce302b6a9e42c2fd591
Evgeniy Karpov <56111469+JohnnyThompson@users.noreply.github.com>
2022-01-10 21:05:59 +0300

    Day 76 Project 22/2 WrapUp

--- 100DayOfSwift/Project22/Project22/ViewController.swift
+++ 100DayOfSwift/Project22/Project22/ViewController.swift
@@ -11,8 +11,11 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
   
   @IBOutlet var distanceReading: UILabel!
+  @IBOutlet var uuidLabel: UILabel!
+  @IBOutlet var circle: UIView!
   
   var locationManager: CLLocationManager?
+  var beaconIsSet = false
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -20,6 +23,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     locationManager?.delegate = self
     locationManager?.requestAlwaysAuthorization()
     view.backgroundColor = .gray
+    circle.layer.cornerRadius = circle.bounds.width / 2
   }
   
   func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -34,7 +38,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
   
   func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
     if let beacon = beacons.first {
+      uuidLabel.text = "\(beacon.uuid)"
       update(distance: beacon.proximity)
+      if !beaconIsSet {
+        showDetectAlert()
+        beaconIsSet = true
+      }
     } else {
       update(distance: .unknown)
     }
@@ -51,24 +60,33 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     UIView.animate(withDuration: 0.8) {
       switch distance {
       case .far:
+        self.circle.transform = CGAffineTransform(scaleX: 0.25, y: 0.25)
         self.view.backgroundColor = UIColor.blue
         self.distanceReading.text = "FAR"
         
       case .near:
+        self.circle.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
         self.view.backgroundColor = UIColor.orange
         self.distanceReading.text = "NEAR"
         
       case .immediate:
+        self.circle.transform = CGAffineTransform(scaleX: 1, y: 1)
         self.view.backgroundColor = UIColor.red
         self.distanceReading.text = "RIGHT HERE"
         
       default:
+        self.circle.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
         self.view.backgroundColor = UIColor.gray
         self.distanceReading.text = "UNKNOWN"
       }
     }
   }
   
+  private func showDetectAlert() {
+    let ac = UIAlertController(title: "Beacon is found", message: nil, preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "OK", style: .default))
+    present(ac, animated: true)
+  }
   
 }
 
